00f2559295f28c8147054cc27ccb9a3af065603d
hainiu <hainiu@example.com>
2023-07-08 20:13:26 +0800

    第二次提交

---
+++ mav_test1/src/main/java/com/hainiuxy/Demo02.java
@@ -0,0 +1,11 @@
+package com.hainiuxy;
+
+import javax.sound.midi.Soundbank;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
+    }
+}
